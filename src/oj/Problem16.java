a73a220749535e9e7149c3c6c16e0285c34ba4c4
zhaoyang <1011509513@qq.com>
2019-10-28 09:41:30 +0800

    解决题目16
    四则运算．使用栈的思想．

--- src/oj/Problem16.java
+++ src/oj/Problem16.java
@@ -1,9 +1,6 @@
 package oj;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * 题目描述:实现一个算法，可以进行任意非负整数的加减乘除组合四则运算。
@@ -23,14 +20,24 @@ public class Problem16 {
 
         while (scan.hasNextLine()) {
             line = scan.nextLine().trim();
+            if(line==null || line.equals("")){
+                continue;
+            }
+
             ArrayList<CalNode> nodeList = new ArrayList<>();//存储所有节点
             StringBuilder builder = new StringBuilder();//缓存数字
 
-            //读取所有节点
+            //1.读取所有节点
             int index=0;
             while (index < line.length()) {
+                if (!isLegalNumeric(String.valueOf(line.charAt(index)))) {
+                    index++;
+                    continue;
+                }
+
                 String word = String.valueOf(line.charAt(index));
                 if (isOperator(word)) {
+                    //flush剩余的数值.
                     nodeList.add(new Operand(Integer.parseInt(builder.toString())));
                     builder = new StringBuilder();
                     nodeList.add(new Operator(word));
@@ -43,14 +50,14 @@ public class Problem16 {
                 //清空builder中的数字
                 if (index >= line.length() && builder.length() > 0) {
                     nodeList.add(new Operand(Integer.parseInt(builder.toString())));
-                    builder = new StringBuilder();
+                    builder = null;
                 }
             }
 
-
-            Stack<CalNode> stack = new Stack<>();
-            index =0 ;
-            boolean isError = false;
+            //2.首先处理乘除两种运算符.
+            LinkedList<CalNode> linkedQueue = new LinkedList<>();
+            index = 0;
+            boolean isError = false;//除数为0，则输出err信息.
             while (index < nodeList.size()) {
                 CalNode curNode = nodeList.get(index);
                 if (curNode instanceof Operator) {
@@ -58,41 +65,54 @@ public class Problem16 {
                     //如果运算符为乘除
                     if (operator.value.equals(Operator.DIVIDE)
                             || operator.value.equals(Operator.MULTIPLY)) {
-                        Operand operand1 = (Operand) stack.pop();
+                        Operand operand1 = (Operand) linkedQueue.pop();
                         Operand operand2 = (Operand) nodeList.get(++index);
                         if (operator.value.equals(Operator.DIVIDE)) {
                             if (operand2.value == 0) {
-                                System.out.println("err");
                                 isError = true;
                                 break;
                             } else {
-                                stack.push(new Operand(operand1.value / operand2.value));
+                                linkedQueue.push(new Operand(operand1.value / operand2.value));
                             }
-                        } else {
-                            stack.push(new Operand(operand1.value * operand2.value));
+                        } else if (operator.value.equals(Operator.MULTIPLY)) {
+                            linkedQueue.push(new Operand(operand1.value * operand2.value));
                         }
                     } else {
-                        stack.push(new Operator(operator.value));
+                        linkedQueue.push(new Operator(operator.value));
                     }
-                }else{
+                } else {
                     Operand operand = (Operand) curNode;
-                    stack.push(operand);
+                    linkedQueue.push(operand);
                 }
                 index++;
             }
 
-            //计算栈中剩余元素的值,只有加减运算符和数值
-            while (stack.size() > 1) {
-                Operand operand2 = (Operand) stack.pop();
-                Operator operator = (Operator) stack.pop();
-                Operand operand1 = (Operand) stack.pop();
 
-                switch (operator.value){
+            //输出错误信息
+            if (isError) {
+                System.out.println("err");
+                continue;
+            }
+
+            //计算栈中剩余元素的值,只有加减运算符和数值
+            while (linkedQueue.size() > 1) {
+                Operand operand1 = (Operand) linkedQueue.pollLast();
+                Operator operator = (Operator) linkedQueue.pollLast();
+                Operand operand2 = (Operand) linkedQueue.pollLast();
+
+                switch (operator.value) {
                     case Operator.PLUS:
+                        linkedQueue.addLast(new Operand(operand1.value + operand2.value));
                         break;
                     case Operator.MINUS:
+                        linkedQueue.addLast(new Operand(operand1.value - operand2.value));
+                        break;
                 }
             }
+
+            //输出最终结果
+            Operand result = (Operand) linkedQueue.pollFirst();
+            System.out.println(result.value);
 
         }
     }
@@ -102,6 +122,31 @@ public class Problem16 {
                 Operator.MINUS.equals(character) ||
                 Operator.MULTIPLY.equals(character) ||
                 Operator.PLUS.equals(character);
+    }
+
+    private static boolean isLegalNumeric(String word) {
+        boolean illegal = word != null && !word.trim().equals("");
+
+        if (!illegal) {
+            return false;
+        }
+
+        boolean isOperator = (word.equals(Operator.MULTIPLY)
+                || word.equals(Operator.DIVIDE)
+                || word.equals(Operator.MINUS)
+                || word.equals(Operator.PLUS));
+
+        if (isOperator) {
+            return true;
+        }
+
+        try {
+            Integer.valueOf(word);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+
     }
 
     abstract static class CalNode{
@@ -127,9 +172,6 @@ public class Problem16 {
             this.value = value;
         }
 
-        public String getValue() {
-            return value;
-        }
     }
 
 }
