591f258f9a175ff3b8fddf6acab5d42c46722d8d
zhaoyang <1011509513@qq.com>
2020-01-02 00:55:29 +0800

    LeetCode Problem29
    不用乘除法，求被除数/除数的商

---
+++ src/leetcode/binary_search/Problem29.java
@@ -0,0 +1,25 @@
+package src.leetcode.binary_search;
+/**
+ * 定两个整数，被除数 dividend 和除数 divisor。将两数相除，要求不使用乘法、除法和 mod 运算符。
+ *
+ * 返回被除数 dividend 除以除数 divisor 得到的商
+ *
+ * 输入: dividend = 10, divisor = 3
+ * 输出: 3
+ *
+ *
+ * dividend:被除数
+ * divisor: 除数
+ *
+ * */
+public class Problem29 {
+    public static void main() {
+
+    }
+
+    public static int divide(int dividend, int divisor) {
+        for(int i=31;i>=){
+
+        }
+    }
+}
