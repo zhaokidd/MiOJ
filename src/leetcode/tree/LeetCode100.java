64cfc4deb7f8723787dfcb9db7ecf104a69ab24b
zhaoyang10 <zhaoyang10@xiaomi.com>
2020-02-17 11:46:25 +0800

    LeetCode100

---
+++ src/leetcode/tree/LeetCode100.java
@@ -0,0 +1,24 @@
+package src.leetcode.tree;
+
+/**
+ * 给定两个二叉树，编写一个函数来检验它们是否相同。
+ * <p>
+ * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的
+ */
+public class LeetCode100 {
+    public static void main(String[] args) {
+
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null || q == null) {
+            return false;
+        } else if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
