84358568c4c1275709bf16260b2190a9e668107e
zhaoyang <zhaoyang10@xiaomi.com>
2020-03-13 18:15:50 +0800

    反转链表
    Signed-off-by:zhaoyang10<zhaoyang10@xiaomi.com>

---
+++ src/leetcode/sort/LeetCode24ReverseNode.java
@@ -0,0 +1,45 @@
+package src.leetcode.sort;
+
+public class LeetCode24ReverseNode {
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+
+        ListNode newHead = head;
+        while (newHead.next != null) {
+            newHead = newHead.next;
+        }
+
+        reverseListInner(head);
+        return newHead;
+    }
+
+    private void reverseListInner(ListNode node) {
+        if (node.next == null) {
+            return;
+        }
+
+        if (node.next.next != null) {
+            reverseListInner(node.next);
+        }
+
+        ListNode temp = node.next;
+        temp.next = node;
+        node.next = null;
+
+    }
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
